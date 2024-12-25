6f6b2dfa4350775390431840a16609f3f2878df0
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-12-25 11:00:17 +0700

    Add files via upload

---
+++ SS21_BAI3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char userInput[100];
+
+    printf("Nhap vao chuoi bat ky: ");
+    fgets(userInput, sizeof(userInput), stdin);
+
+    FILE *file = fopen("bt01.txt", "a"); // M? file ? ch? d? ghi thêm
+    if (file != NULL) {
+        fprintf(file, "%s", userInput);
+        fclose(file);
+        printf("Chuoi da duoc ghi them vao file bt01.txt\n");
+    } else {
+        printf("Khong the mo file de ghi.\n");
+    }
+
+    return 0;
+}
+
